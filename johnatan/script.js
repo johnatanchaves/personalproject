95e815f47193e25f522055ee08ee8b8e5f9f4646
Johnatan Chaves <johnechaves15@gmail.com>
2023-05-04 23:23:51 -0300

    meu portfolio

---
+++ johnatan/script.js
@@ -0,0 +1,27 @@
+// FUNÇÃO ATIVA LETRA PARA DIGITAR ALTOMATICO
+
+
+function ativaLetra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((Letra, i)=>{
+setTimeout(()=>{
+    elemento.innerHTML += Letra;
+      }, 75 * i);
+
+    });
+}
+const titulo = document.querySelector('.digitando');
+ ativaLetra(titulo);
+
+
+
+
+const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria')
+
+ativaMenu.addEventListener('click', ()=>{
+  ativaMenu.classList.toggle('fa-x')
+  navMenu.classList.toggle('ativado')
+})
+
